5414714db4eacf7f26cdecb64258838d0abdf4eb
Rizvan <xl92@mail.ru>
2022-11-25 02:57:49 +0300

    finish section slider

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,36 @@
-const num = 55;
+$(document).ready(function () {
+    $('.slider__view').slick({
+        lazyLoad: 'ondemand',/* 'ondemand' or 'progressive' */
+        slidesToShow: 4,/* по скольку показывать */
+        slidesToScroll: 2,/* по сколькуо листать */
+        infinite: true,/* бесконечная прокрутка отключена */
+        rows: 1,/* задает количество рядов */
+        arrows: false,
+        dots: true,
+
+        responsive: [/* отзывчивый */
+            {
+                breakpoint: 1300,
+                settings: {
+                    slidesToShow: 7,
+                    slidesToScroll: 5,
+                    infinite: false,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
